89d67524e208e46f82333101c5eb570f61bd4958
ttasjwi <ttasjwi920@gmail.com>
2022-11-08 17:21:58 +0900

    [USER-SERVICE] 사용자 Entity, Repository 구현

---
+++ user-service/src/main/java/com/ttasjwi/userservice/repository/UserEntity.java
@@ -0,0 +1,37 @@
+package com.ttasjwi.userservice.repository;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class UserEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false)
+    private String encryptedPassword;
+
+    @Builder
+    public UserEntity(String email, String name, String userId, String encryptedPassword) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+        this.encryptedPassword = encryptedPassword;
+    }
+}
